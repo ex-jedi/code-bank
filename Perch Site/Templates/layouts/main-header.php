cf9548dc981540bb476fc0adc0cb176fa64c58eb
Mark Phoenix <mark@pixelsmiths.co.uk>
2018-01-06 10:43:37 +0000

    Adding schema local business to main sire wrapper.

--- Perch Site/Templates/layouts/main-header.php
+++ Perch Site/Templates/layouts/main-header.php
@@ -32,7 +32,7 @@
 <?php perch_content('Analytics'); ?>
 </head>
 	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>">
-		<div class="wrapper <?php perch_page_attribute('wrapperClass', array('template' => 'bits.html'));?>">
+		<div itemscope itemtype="http://schema.org/LocalBusiness" class="wrapper <?php perch_page_attribute('wrapperClass', array('template' => 'bits.html'));?>">
 			<header class="main-header">
 				<nav class="main-nav">
 					<?php perch_pages_navigation(array(
