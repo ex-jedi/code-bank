45f988ca161eb7ff86d9647e705077351d5d9f2c
Mark Phoenix <mark@pixelsmiths.co.uk>
2018-07-17 18:49:34 +0100

    Mostly Perch UI stuff.

---
+++ Perch Site/Addons & Plugins/Ancillary Addons/jaygeorge_perch_admin_style/admin.php
@@ -0,0 +1,18 @@
+<?php
+    $this->register_app('jaygeorge_perch_admin_style', 'Admin Style', 1, 'An app that styles the Perch admin', 1.0);
+    $this->require_version('jaygeorge_perch_admin_style', '2.8');
+    $this->add_setting('jaygeorge_perch_admin_style_external_font_stylesheet', 'External Font Stylesheet', 'text', false,'','This is an optional stylesheet include for loading something like Typekit or Google Fonts e.g. https://use.typekit.net/dsl0pss.css');
+    $this->add_create_page('jaygeorge_perch_admin_style', 'edit');
+
+    $API = new PerchAPI(1.0, 'jaygeorge_perch_admin_style');
+
+    $Perch = Perch::fetch();
+    // We need to separate variables.css out so that the login.css can make use of it (which is loaded outside the app, so it can be available before the admin is loaded)
+    $Perch->add_css($API->app_path() . '/variables.css');
+    $Perch->add_css($API->app_path() . '/standard-admin.css');
+    $Perch->add_css('/perch/addons/plugins/ui/custom-admin.css');
+    $Perch->add_css($API->get('Settings')->get('jaygeorge_perch_admin_style_external_font_stylesheet')->val());
+
+    // Make sure favicon isn't cached
+    $favicon_version = '<link rel="shortcut icon" href="/perch/addons/plugins/ui/favicon.ico?v=' . filemtime($_SERVER['DOCUMENT_ROOT'].'/perch/addons/plugins/ui/favicon.ico') . '">';
+    $Perch->add_head_content($favicon_version);
---
+++ Perch Site/Addons & Plugins/Ancillary Addons/jaygeorge_perch_admin_style/jaygeorge_perch_admin_styleSync_Thing.class.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * A class representing a single Thing item
+ */
+class jaygeorge_perch_admin_styleSync_Thing extends PerchAPI_Base
+{
+    protected $table     = 'jaygeorge_perch_admin_style_settings';
+	protected $pk        = 'admincssID';
+}
