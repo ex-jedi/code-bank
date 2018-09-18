18bd3dd28855c7f1a79cb7b01b0344e5c8d6b6c1
Mark Phoenix <mark@pixelsmiths.co.uk>
2018-09-18 19:00:58 +0100

    Bunch of stuff

---
+++ Perch Site/Addons & Plugins/Shop Files/layouts/main.footer.php
@@ -0,0 +1,25 @@
+
+	<footer class="main-footer">
+
+				<div class="social-media">
+
+					<a class="instagram"  title="Follow me on Instagram" href="<?php perch_content('Footer Instagram link'); ?>"></a>
+					<a class="twitter" title="Follow me on Twitter" href="<?php perch_content('Footer Twitter link'); ?>"></a>
+					<a class="tumblr" title="Follow me on Tumblr" href="<?php perch_content('Footer Tumblr link'); ?>"></a>
+					<a class="fivehundred" title="Follow me on 500px" href="<?php perch_content('500px link'); ?>"></a>
+				</div>
+
+				<div class="legal">
+					<p class="copyright" >&copy; Longshot Photography  <?php echo date("Y"); ?></p>
+					<p class="terms"><a href="/terms.php">Terms of use</a></p>
+					<p class="privacy"><a href="/privacy-policy.php">Privacy policy</a></p>
+				</div>
+
+				<div class="promo">
+					<p>
+						Built by <a href="http://pixelsmiths.co.uk">Pixelsmiths</a>
+					</p>
+				</div>
+
+
+	</footer>
--- Perch Site/Utility Files/gulp/gulpfile.js
+++ Perch Site/Utility Files/gulp/gulpfile.js
@@ -1,53 +1,55 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename");
-var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let autoprefixer = require("gulp-autoprefixer");
+let uglify = require("gulp-uglify");
+let rename = require("gulp-rename");
+let sourcemaps = require("gulp-sourcemaps");
+let browserSync = require("browser-sync").create();
 
-gulp.task('sass', function(){
-  return gulp.src('sass/*.scss')
+gulp.task("sass", function() {
+  return gulp
+    .src("sass/*.scss")
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', function(err) {
-            console.error(err.message);
-            browserSync.notify(err.message, 3000); // Display error in the browser
-            this.emit('end'); // Prevent gulp from catching the error and exiting the watch process
-        }))
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('css'))
+    .pipe(
+      sass({ outputStyle: "compressed" }).on("error", function(err) {
+        console.error(err.message);
+        browserSync.notify(err.message, 6000); // Display error in the browser
+        this.emit("end"); // Prevent gulp from catching the error and exiting the watch process
+      })
+    )
+    .pipe(autoprefixer("last 2 versions"))
+    .pipe(sourcemaps.write("../css"))
+    .pipe(gulp.dest("css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('compress', function () {
+gulp.task("compress", function() {
   gulp
     .src("scripts/js/*.js")
     .pipe(sourcemaps.init())
-    .pipe(uglify().on("error", function(err) {
+    .pipe(
+      uglify().on("error", function(err) {
         console.error(err.message);
-        browserSync.notify(err.message, 3000); // Display error in the browser
+        browserSync.notify(err.message, 6000); // Display error in the browser
         this.emit("end"); // Prevent gulp from catching the error and exiting the watch process
-      }))
-    .pipe(sourcemaps.write("./maps"))
+      })
+    )
+    .pipe(sourcemaps.write("../minified"))
     .pipe(rename({ suffix: "-min" }))
     .pipe(gulp.dest("scripts/minified/"));
 });
 
-
-gulp.task('browserSync', function() {
-    browserSync.init({
-        proxy: 'xxxx',
-        host: 'xxxx',
-        open: 'external'
-    });
+gulp.task("browserSync", function() {
+  browserSync.init({
+    proxy: "xxxx"
+  });
 });
 
-gulp.task('watch',['browserSync', 'sass', 'compress'], function(){
-    gulp.watch('sass/*.scss', ['sass']);
-    gulp.watch('scripts/js/*.js', ['compress']);
-    gulp.watch('perch/templates/**/*.*', browserSync.reload);
-    gulp.watch('*.php', browserSync.reload);
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('scripts/minified/*.js', browserSync.reload);
-})
+gulp.task("watch", ["browserSync", "sass", "compress"], function() {
+  gulp.watch("sass/*.scss", ["sass"]);
+  gulp.watch("scripts/js/*.js", ["compress"]);
+  gulp.watch("perch/templates/**/*.*", browserSync.reload);
+  gulp.watch("*.php", browserSync.reload);
+  gulp.watch("*.html", browserSync.reload);
+  gulp.watch("scripts/minified/*.js", browserSync.reload);
+});
