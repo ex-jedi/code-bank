101c0da23d10bf11170740f2e941615ad1d8559d
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-09-24 20:09:43 +0100

    Updating Perch main footer file

--- Perch Site/Templates/layouts/main-footer.php
+++ Perch Site/Templates/layouts/main-footer.php
@@ -1,19 +1,27 @@
 <footer class="main-footer">
-	<div class="footer-legal">
-		<p class="copyright" >&copy; <?php echo date("Y"); ?> Site Name</p>
-		<p class="terms"><a href="/terms.php">Terms of use</a></p>
-		<p class="privacy"><a href="/privacy-policy.php">Privacy policy</a></p>
+	<div class="footer-social-media">
+		<a href="#" class="footer-social-media-link">
+			<img class="footer-social-media-icon" src="/images/instagram-icon.svg">
+		</a>
+		<a href="#" class="footer-social-media-link">
+			<img class="footer-social-media-icon" src="/images/twitter-icon.svg">
+		</a>
 	</div>
-	<div class="promo">
+	<div class="footer-legal">
+		<p class="copyright" >&copy; <?php echo date("Y"); ?> KEN LONG</p>
+		<p class="privacy"><a href="/privacy-policy.php">PRIVACY &amp; COOKIE PRIVACY POLICY</a></p>
+		<p class="terms"><a href="/terms.php">TERMS OF USE</a></p>
+	</div>
+	<div class="footer-promo">
 		<p>
-			<a href="https://pixelsmiths.co.uk">Built by Pixelsmiths</a>
+			WEB DESIGN & DEVELOPMENT, BRAND DESIGN & ILLUSTRATION BY <a href="" class="promo-link">phoenix & phoenix</a>
 		</p>
 	</div>
 </footer>
 </div> <!-- wrapper div -->
-<!-- TODO: Check below for latest versions -->
-<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script><!-- Jquery CDN -->
-<script>window.jQuery || document.write('<script src="/scripts/minified/jquery-3.1.0.min.js"><\/script>')</script><!-- Jquery Backup -->
+<!-- TODO: Change Vue to production  version -->
+<script type="text/javascript" src="/scripts/minified/vue-min.js" ></script>
+<!-- General JS file -->
 <script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Local universal JS files -->
 </body>
 </html>
