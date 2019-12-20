3acb225e68a2f476cdad70f473da3a014d525b8f
Mark Phoenix <mark@pixelsmiths.co.uk>
2019-12-20 15:42:36 +0000

    Changing homepage template

--- Perch Site/whole site files/index.php
+++ Perch Site/whole site files/index.php
@@ -1,10 +1,9 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
-	<h1>Hello World</h1>
-	<section>
+<h1>Ahoy</h1>
 	  <?php perch_content('Placeholder'); ?>
-	</section>
 </main>
 <?php perch_layout('main-footer'); ?>
+<!-- TODO: Remode debug -->
 <!-- <?php PerchUtil::output_debug(); ?> -->
