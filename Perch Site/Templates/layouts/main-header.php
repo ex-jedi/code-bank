f88216f3f015b8889b24b971866b92521c8cbbc9
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-18 13:32:39 +0100

    Removing Vue from main header

--- Perch Site/Templates/layouts/main-header.php
+++ Perch Site/Templates/layouts/main-header.php
@@ -36,7 +36,7 @@
 
 </head>
 	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>">
-				<div id="vue-wrapper" class="site-wrapper" :class="{'hide-outline': outlineHide}" @keydown.9="outlineHide = !outlineHide" itemscope itemtype="http://schema.org/LocalBusiness">
+				<div  class="site-wrapper" itemscope itemtype="http://schema.org/LocalBusiness">
 			<header class="main-header">
 				<!-- Skip to main content link  -->
 				<a class="show-on-focus"  href="#main-content">Skip to main content</a>
