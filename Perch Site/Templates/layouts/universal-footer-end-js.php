4478501987d21deffef6728c29b51f4ff8f618d2
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-18 11:31:44 +0100

    Changing universal js file name to main

--- Perch Site/Templates/layouts/universal-footer-end-js.php
+++ Perch Site/Templates/layouts/universal-footer-end-js.php
@@ -1,6 +1,3 @@
-<!-- TODO: Change Vue to production  version -->
-<script type="text/javascript" src="/scripts/minified/vue-min.js" ></script>
-<!-- General JS file -->
-<script type="text/javascript" src="/scripts/minified/universal-min.js"></script><!-- Local universal JS files -->
+<script type="text/javascript" src="/scripts/minified/main-min.js"></script><!-- Local universal JS files -->
 </body>
 </html>
