55c2cebc0ed9f47e2a2b0aa91da8bddcd29e7af8
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-18 13:05:21 +0100

    Adding focus outline

--- Perch Site/whole site files/scripts/js/main.js
+++ Perch Site/whole site files/scripts/js/main.js
@@ -22,3 +22,26 @@ cookieWarningButton.addEventListener(
   },
   { once: true }
 );
+
+// *=========================================
+// ** Accessibility  **
+// *=========================================
+
+// * Adding focus outline class when tab key is used
+function handleFirstTab(e) {
+  if (e.keyCode === 9) {
+    document.body.classList.add('user-is-tabbing');
+
+    window.removeEventListener('keydown', handleFirstTab);
+    window.addEventListener('mousedown', handleMouseDownOnce);
+  }
+}
+
+function handleMouseDownOnce() {
+  document.body.classList.remove('user-is-tabbing');
+
+  window.removeEventListener('mousedown', handleMouseDownOnce);
+  window.addEventListener('keydown', handleFirstTab);
+}
+
+window.addEventListener('keydown', handleFirstTab);
