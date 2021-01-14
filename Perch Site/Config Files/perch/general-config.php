ce993760ed6878b9bb05ca9965a961e01fff9570
Mark <mark@pixelsmiths.co.uk>
2021-01-14 15:41:19 +0000

    Removing old config files

---
+++ Perch Site/Config Files/perch/general-config.php
@@ -0,0 +1,25 @@
+<?php
+    define('PERCH_LICENSE_KEY', 'P21602-RYY608-BHZ161-RLB241-LVV027');
+
+    define("PERCH_DB_USERNAME", 'master_control');
+    define("PERCH_DB_PASSWORD", 'roygbiv');
+    define("PERCH_DB_SERVER", "localhost");
+    define("PERCH_DB_DATABASE", "newworld_local");
+    define("PERCH_DB_PREFIX", "perch3_");
+
+    define('PERCH_TZ', 'UTC');
+
+    define('PERCH_EMAIL_FROM', 'mark@pixelsmiths.co.uk');
+    define('PERCH_EMAIL_FROM_NAME', 'Mark Phoenix');
+
+    define('PERCH_LOGINPATH', '/perch');
+    define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
+    define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
+
+    define('PERCH_RESFILEPATH', PERCH_PATH . DIRECTORY_SEPARATOR . 'resources');
+    define('PERCH_RESPATH', PERCH_LOGINPATH . '/resources');
+
+    define('PERCH_HTML5', true);
+
+    define('PERCH_TEMPLATE_FILTERS', true);
+    define('PIPIT_PHONE_COUNTRY', 'GB');
