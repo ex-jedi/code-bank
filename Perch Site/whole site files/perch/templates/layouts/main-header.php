09be2728d487a6cdf48dfc92f2906d0d2c8b18eb
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-03-05 13:48:43 +0000

    Adding SVG to main header

--- Perch Site/whole site files/perch/templates/layouts/main-header.php
+++ Perch Site/whole site files/perch/templates/layouts/main-header.php
@@ -6,6 +6,8 @@
 	<link rel="stylesheet" href="/css/main.css?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/css/main.css');?>">
 	<?php $page_css_file	=  perch_page_attribute('css', array('template' => 'bits.html'), true); ?>
 	<link rel="stylesheet" href="<?php echo $page_css_file ?>?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].$page_css_file);?>">
+	<!-- Typekit  -->
+	<link rel="stylesheet" href="https://use.typekit.net/xvs0zel.css">
 	<!-- Perch Meta -->
 	<?php
 	$domain        = 'https://'.$_SERVER["HTTP_HOST"];
@@ -15,9 +17,9 @@
 										    'add-trailing-slash' => false,
 										    'include-domain'     => true,
 										), true);
-	$mainsitename      = "SITE NAME";
-	$pagetitlename = " - SITE NAME";
-	$sharing_image = '/images/default-sharing-image.jpg';
+	$mainsitename      = "phoenix & phoenix";
+	$pagetitlename = " - phoenix & phoenix";
+	$sharing_image = '/images/default_fb_image.jpg';
 
 	PerchSystem::set_var('domain',$domain);
 	PerchSystem::set_var('mainurl',$mainurl);
@@ -29,19 +31,26 @@
 		'template' => 'default.html'
 	));
 	?>
-
+	<?php perch_content("Analytics"); ?>
 </head>
-<!-- Google Analytics -->
-<?php perch_content('Analytics'); ?>
-
+	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>">
 <!-- Cookie Warning -->
 <?php perch_content("Cookie Warning"); ?>
-	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>">
 				<div class="site-wrapper" itemscope itemtype="http://schema.org/LocalBusiness">
-			<header class="main-header">
+			<header class="header main-header">
 				<!-- Skip to main content link  -->
 				<a class="show-on-focus"  href="#main-content">Skip to main content</a>
+				<!-- For SVG logo. Delete if not needed -->
+				<!-- <div class="main-logo-wrapper">
+					<a class="main-logo-wrapper" itemprop="url" href="<?php echo $domain ?>">
+					<img class="hidden-main-logo" aria-hidden="true" itemprop="logo" src="/images/main-logo.png" alt="phoenix and phoenix main logo">
+					<?php perch_content("Main Logo"); ?>
+				</a>
+				</div> -->
 					<?php perch_pages_navigation(array(
 							'hide-extensions' => true,
+							'levels'    => 1
 					)); ?>
+
+        <?php perch_content("Header Content"); ?>
 			</header>
