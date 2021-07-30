867b577f377ed5d681909d14251683fba82e837a
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-07-30 15:22:35 +0100

    Paragraph

--- Perch Site Starter/scripts/js/animations.js
+++ Perch Site Starter/scripts/js/animations.js
@@ -8,6 +8,10 @@
 import { gsap } from 'gsap';
 import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin';
+import { SplitText } from 'gsap/SplitText';
+
 
 // *=========================================
 // ** GSAP  **
