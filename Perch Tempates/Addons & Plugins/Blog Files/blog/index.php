5ea421de2f38ba26be73b1cab7eec9efee1045a2
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-08-05 15:13:02 +0100

    Cats template

--- Perch Tempates/Addons & Plugins/Blog Files/blog/index.php
+++ Perch Tempates/Addons & Plugins/Blog Files/blog/index.php
@@ -1,6 +1,15 @@
 <?php include('../perch/runtime.php'); ?>
-<?php perch_layout('blog-header'); ?>
+<?php perch_layout('heads/blog-index-head'); ?>
+<?php perch_layout('headers/blog-index-header'); ?>
   <main class="blog-main-content blog-index-main-content" id="main-content">
+  <?php perch_content("Blog Index Heading"); ?>
+      <?php
+      $categories = perch_blog_categories(array(
+        'template' => 'index_category_link.html'
+      ));
+      ?>
     <?php perch_blog_recent_posts(5); ?>
+    <?php perch_content("Instagram Feed"); ?>
   </main>
-<?php perch_layout('blog-footer'); ?>
+<?php perch_layout('footers/main-footer'); ?>
+<?php perch_layout('footer-ends/blog-footer-end'); ?>
